3fb156ecc44fc322e6cc6ece5e5a95f9ffd1960f
knt5 <knt5@users.noreply.github.com>
2016-05-05 13:34:42 +0900

    Add scss-lint task

---
+++ gulp/task/lint.js
@@ -0,0 +1,9 @@
+var gulp   = require('gulp');
+var scsslint = require('gulp-scss-lint');
+
+gulp.task('scss-lint', function() {
+	return gulp.src('src/scss/**/*.scss')
+		.pipe(scsslint());
+});
+
+gulp.task('lint', ['scss-lint']);
