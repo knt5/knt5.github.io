e8f194568fc40e2b8cdb2716759de982007f8eb8
knt5 <knt5@users.noreply.github.com>
2016-11-02 22:04:13 +0900

    Change the document ready code style

--- src/js/index/sparkles.js
+++ src/js/index/sparkles.js
@@ -1,28 +1,25 @@
-(function() {
-	$(document).ready(function() {
-		$('#js-header-image').sparkle({
-			direction: 'down',
-			speed: 0.4,
-			fadeSpeed: 3000,
-			overlap: 5
-		});
-		
-		$('#js-profile-lang-chart-label-c').sparkle({
-			color: ['#ff9', '#fff'],
-			direction: 'up',
-			speed: 1,
-			count: 150,
-			fadeSpeed: 3000,
-			overlap: 30
-		});
-		
-		$('#js-profile-lang-chart-label-js').sparkle({
-			color: ['#ff9', '#fff'],
-			speed: 1,
-			count: 180,
-			fadeSpeed: 3000,
-			overlap: 10
-		});
-		
+$(function() {
+	$('#js-header-image').sparkle({
+		direction: 'down',
+		speed: 0.4,
+		fadeSpeed: 3000,
+		overlap: 5
 	});
-})();
+	
+	$('#js-profile-lang-chart-label-c').sparkle({
+		color: ['#ff9', '#fff'],
+		direction: 'up',
+		speed: 1,
+		count: 150,
+		fadeSpeed: 3000,
+		overlap: 30
+	});
+	
+	$('#js-profile-lang-chart-label-js').sparkle({
+		color: ['#ff9', '#fff'],
+		speed: 1,
+		count: 180,
+		fadeSpeed: 3000,
+		overlap: 10
+	});
+});
