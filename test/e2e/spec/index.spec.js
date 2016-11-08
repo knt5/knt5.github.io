35344f82ce5a017d1efb1966e786d44dcbb57ec7
knt5 <knt5@users.noreply.github.com>
2016-11-09 00:55:21 +0900

    Add index spec

---
+++ test/e2e/spec/index.spec.js
@@ -0,0 +1,14 @@
+const IndexPage = require('../page/IndexPage');
+
+describe('index page', () => {
+	let indexPage;
+	
+	before(() => {
+		indexPage = new IndexPage();
+		indexPage.open();
+	});
+	
+	it('test', () => {
+		
+	});
+});
