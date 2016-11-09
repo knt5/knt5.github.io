1f77b7a79cfc42e586a4195bb977ab9a4bb9b880
knt5 <knt5@users.noreply.github.com>
2016-11-09 23:34:50 +0900

    Refactor

--- test/smoke/index.smoke.js
+++ test/smoke/index.smoke.js
@@ -1,16 +1,15 @@
-const expect = require('chai').expect;
 const request = require('supertest');
 const cheerio = require('cheerio');
 
-describe('index page', () => {
+describe('Smoke testing: index page', () => {
 	const base = 'http://localhost:8000';
 	const path = '/';
 	const ignoreLinks = new Set([
 		'https://www.facebook.com/knt5f',
 	]);
 	const maxRidirects = 2;
-	
-	it('Every link\'s http response is 200', (done) => {
+
+	it('Every link response is 200', (done) => {
 		request(base)
 			.get(path)
 			.end((err, res) => {
@@ -19,20 +18,20 @@ describe('index page', () => {
 				} else {
 					const $ = cheerio.load(res.text);
 					const links = [];
-					
+
 					$('a').each((i, el) => {
 						const link = $(el).prop('href');
 						if (!ignoreLinks.has(link)) {
 							links.push(link);
 						}
 					});
-					
+
 					let count = 0;
 					testLink(0);
 					function testLink(i) {
 						const link = links[i];
 						let url;
-						
+
 						if (link.match(/^http[s]?:\/\//)) {
 							url = link;
 						} else if (link.startsWith('/')) {
@@ -40,16 +39,16 @@ describe('index page', () => {
 						} else {
 							url = base + path + link;
 						}
-						
+
 						console.log(`      ${url}`);
-						
+
 						request(url)
 							.head('')
 							.redirects(maxRidirects)
 							.expect(200)
-							.end((err, res) => {
-								if (err) {
-									done(err);
+							.end((error) => {
+								if (error) {
+									done(error);
 								} else {
 									count++;
 									if (count < links.length) {
