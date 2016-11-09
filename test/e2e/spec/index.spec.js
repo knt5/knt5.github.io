c614ebdbabfc2267989f4340850a6ed991a5efeb
knt5 <knt5@users.noreply.github.com>
2016-11-09 23:35:33 +0900

    Add screenshot saving

--- test/e2e/spec/index.spec.js
+++ test/e2e/spec/index.spec.js
@@ -1,14 +1,16 @@
+// const expect = require('chai').expect;
 const IndexPage = require('../page/IndexPage');
 
 describe('index page', () => {
 	let indexPage;
-	
+
 	before(() => {
 		indexPage = new IndexPage();
 		indexPage.open();
 	});
-	
-	it('test', () => {
-		
+
+	it('Render the page on any browsers. (This is not a test)', () => {
+		browser.saveDocumentScreenshot(
+			`test/e2e/result/${browser.desiredCapabilities.browserName}.png`);
 	});
 });
