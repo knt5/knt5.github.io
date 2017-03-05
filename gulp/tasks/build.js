677918e37a44bc8cfe2f54ade1fa324db3a1806b
knt5 <knt5@users.noreply.github.com>
2017-03-05 22:21:04 +0900

    Add JavaScript building to the build task

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,12 +1,24 @@
 const fs = require('fs');
 const path = require('path');
 const glob = require('glob');
+
+// Basic
 const gulp = require('gulp');
-const mustache = require('gulp-mustache');
 const plumber = require('gulp-plumber');
 const rename = require('gulp-rename');
+
+// HTML
+const mustache = require('gulp-mustache');
 const htmlmin = require('gulp-htmlmin');
 
+// JavaScript
+const webpack = require('webpack');
+const gulpWebpack = require('gulp-webpack');
+const named = require('vinyl-named');
+
+// CSS
+
+//=============================================================================
 const config = {
 	html: {
 		base: 'src/html/',
@@ -15,35 +27,49 @@ const config = {
 			removeComments: true,
 		},
 	},
+	js: {
+		base: 'src/js/',
+	},
+	tmp: 'gulp/tmp/'
 };
 
-gulp.task('build', () => {
+//=============================================================================
+gulp.task('build', (done) => {
 	
-	//-------------------------------------------------------------------------
-	// Javascript
-	
-	
-	//-------------------------------------------------------------------------
-	// CSS
-	
-	
-	//-------------------------------------------------------------------------
-	// HTML
-	const partials = getPartials();
-	return gulp.src('src/html/views/**/app.html')
-		.pipe(plumber())
-		.pipe(mustache({
-			css: '',
-			javascript: '',
-		}, {}, partials))
-		.pipe(rename(toDirBase))
-		.pipe(htmlmin(config.html.min))
-		.pipe(gulp.dest('./'));
+	gulp.src(`${config.js.base}/views/**/app.js`)  // JavaScript
+		.pipe(named((file) => {
+			return 'views/' + path.dirname(file.relative);
+		}))
+		.pipe(gulpWebpack({
+			module: {
+				loaders: [{
+					loader: 'babel-loader',
+					query: { presets: ['es2015'] }
+				}],
+			},
+		}))
+		.pipe(gulp.dest(config.tmp))
+		.on('end', () => {
+			// Get mustache partials
+			const partials = getPartials();
+			
+			// Generate HTML
+			gulp.src(`${config.html.base}/views/**/app.html`)
+				.pipe(plumber())
+				.pipe(mustache({
+					css: '',
+					javascript: '',
+				}, {}, partials))
+				.pipe(rename(toBase))
+				//.pipe(htmlmin(config.html.min))
+				.pipe(gulp.dest('./'))
+				.on('end', () => done());
+		});
 });
 
 //=============================================================================
 // Rename a file to base name of the parent directory (for gulp-rename)
-function toDirBase(filePath) {
+function toBase(filePath) {
 	filePath.basename = path.basename(filePath.dirname);
 	filePath.dirname = '';
 }
@@ -51,16 +77,22 @@ function toDirBase(filePath) {
 //=============================================================================
 // Get mustache partial file data
 function getPartials() {
-	const base = config.html.base;
 	const partials = {};
 	let key;
 	
-	glob.sync('src/**/*.html').forEach((filePath) => {
-		if (filePath.startsWith(base)) {
-			if (path.basename(filePath) !== 'app.html') {
-				key = filePath.replace(base, '');
-				partials[key] = fs.readFileSync(filePath).toString();
-			}
+	// JavaScript
+	glob.sync(`${config.js.base}/**/*.js`).forEach((filePath) => {
+		if (path.basename(filePath) === 'app.js') {
+			key = path.dirname(filePath).replace(config.js.base, '') + '.js';
+			partials[key] = fs.readFileSync(config.tmp + key).toString();
+		}
+	});
+	
+	// HTML
+	glob.sync(`${config.html.base}/**/*.html`).forEach((filePath) => {
+		if (path.basename(filePath) !== 'app.html') {
+			key = filePath.replace(config.html.base, '');
+			partials[key] = fs.readFileSync(filePath).toString();
 		}
 	});
 	
