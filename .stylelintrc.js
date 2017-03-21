6cbde847c19a6bb3e14a56c91328c01b8532adce
knt5 <knt5@users.noreply.github.com>
2017-03-21 23:09:31 +0900

    Add a no-eol-whitespace option to .stylelintrc

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,5 +2,11 @@ module.exports = {
 	extends: './node_modules/stylelint-config-standard',
 	rules: {
 		indentation: 'tab',
+		'no-eol-whitespace': [
+			true,
+			{
+				ignore: ['empty-lines'],
+			}
+		],
 	}
 };
