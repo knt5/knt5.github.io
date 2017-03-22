8080d1fb0b5f5441b1f705dc85fe95e34f39eb70
knt5 <knt5@users.noreply.github.com>
2017-03-22 23:12:17 +0900

    Add rule-empty-line-before to .stylelintrc

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,11 +2,11 @@ module.exports = {
 	extends: './node_modules/stylelint-config-standard',
 	rules: {
 		indentation: 'tab',
-		'no-eol-whitespace': [
-			true,
-			{
-				ignore: ['empty-lines'],
-			}
-		],
+		'no-eol-whitespace': [true, {
+			ignore: ['empty-lines'],
+		}],
+		'rule-empty-line-before': ['always', {
+			ignore: ['after-comment', 'inside-block'],
+		}],
 	}
 };
