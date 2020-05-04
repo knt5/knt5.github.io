5d4aa374e48dfd766fd305f4c4e698e2d32e02ff
knt5 <knt5@users.noreply.github.com>
2020-05-05 00:24:46 +0900

    refactor: remove request from PorfolioApiError

--- portfolio/src/errors/api/PortfolioApiError.ts
+++ portfolio/src/errors/api/PortfolioApiError.ts
@@ -1,26 +1,11 @@
 import ApiError from '@/errors/api/ApiError';
-import PortfolioApiRequest from '@/models/api/portfolio/PortfolioApiRequest';
-import PortfolioApiRequestBody from '@/models/api/portfolio/PortfolioApiRequestBody';
 import PortfolioApiResponse from '@/models/api/portfolio/PortfolioApiResponse';
 
 /** PortfolioApiError */
 export default class PortfolioApiError<
-	T extends PortfolioApiRequest,
-	B extends PortfolioApiRequestBody,
 	R extends PortfolioApiResponse
-> extends ApiError<
-	PortfolioApiRequest,
-	PortfolioApiRequestBody,
-	PortfolioApiResponse
-> {
-	/** Constructor */
-	constructor(
-		status: number,
-		url?: string,
-		response?: R,
-		request?: T,
-		requestBody?: B
-	) {
-		super(`PortfolioApiError`, status, url, response, request, requestBody);
+> extends ApiError<PortfolioApiResponse> {
+	constructor(status: number, url?: string, response?: R | string) {
+		super(`PortfolioApiError`, status, url, response);
 	}
 }
