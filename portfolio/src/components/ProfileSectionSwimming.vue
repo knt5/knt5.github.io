eee6b62cece3263c30dc55c2dfa983c8032ce24e
knt5 <knt5@users.noreply.github.com>
2020-05-05 20:35:39 +0900

    feat: multiple swimming columns

--- portfolio/src/components/ProfileSectionSwimming.vue
+++ portfolio/src/components/ProfileSectionSwimming.vue
@@ -3,9 +3,12 @@
 	.swimming
 		.sub-section-title.title Swimming
 		.sub-section-content
-			.record-box
-				.record(v-for="record in records" :key="record.year")
-					| {{ record.year }}: {{ record.distance | digit }}m ({{ feet(record.distance) | digit }}ft)
+			.record-column(v-for="(columnRecords, index) in records" :key="index")
+				.record-row(v-for="record in columnRecords" :key="record.year")
+					.record-year {{ record.year }}
+					.record : {{ record.distance | digit }}m ({{ feet(record.distance) | digit }}ft)
+					.attention-mark(v-show="record.year === 2020") *
+		.attention * 2020: 🦠 closed all swimming pools
 </template>
 
 <script lang="ts">
@@ -15,6 +18,9 @@ import ApiRepository from '@/repositories/api/ApiRepository';
 import SwimmingRepository from '@/repositories/api/portfolio/SwimmingRepository';
 import GetSwimmingApiResponse from '@/models/api/portfolio/swimming/GetSwimmingApiResponse';
 
+/** Max rows a column */
+const maxRow = 5;
+
 /** Swimming */
 @Component({})
 export default class ProfileSectionSwimming extends Vue {
@@ -22,12 +28,23 @@ export default class ProfileSectionSwimming extends Vue {
 		SwimmingRepository
 	>(ApiRepository.Name.Swimming);
 
-
 	private response: GetSwimmingApiResponse | null = null;
 
-	private get records(): GetSwimmingApiResponse.Record[] {
-		// TODO: to multiple columns
-		return this.response && this.response.records ? this.response.records : [];
+	private get records(): GetSwimmingApiResponse.Record[][] {
+		const records = [];
+
+		if (this.response) {
+			let columnIndex = -1;
+			this.response.records.forEach((record, index) => {
+				if (index % maxRow === 0) {
+					columnIndex++;
+					records[columnIndex] = [];
+				}
+				records[columnIndex].push(record);
+			});
+		}
+
+		return records;
 	}
 
 	private feet(meter: number): number {
@@ -52,11 +69,35 @@ export default class ProfileSectionSwimming extends Vue {
 	margin-top: 25px;
 }
 
-.record-box {
+.record-column {
 	display: inline-block;
+	padding: 0 25px;
+	vertical-align: top;
+}
+
+.record-row {
+	text-align: left;
 }
 
 .record {
-	text-align: left;
+	display: inline-block;
+}
+
+.record-year {
+	display: inline-block;
+	font-weight: bold;
+	font-size: 16px;
+}
+
+.attention-mark {
+	display: inline-block;
+	padding-left: 4px;
+}
+
+.attention {
+	margin-top: 15px;
+	color: #999;
+	font-size: 14px;
+	text-align: center;
 }
 </style>
