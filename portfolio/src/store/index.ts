0196084365b2395e93d71469517cee5bcfedec74
knt5 <knt5@users.noreply.github.com>
2020-05-18 01:20:19 +0900

    chore: set vuex global config.rawError to true

--- portfolio/src/store/index.ts
+++ portfolio/src/store/index.ts
@@ -1,11 +1,12 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import { config } from 'vuex-module-decorators';
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
-	state: {},
-	mutations: {},
-	actions: {},
-	modules: {},
-});
+// Set rawError to true by default on all @Action decorators
+// to throw Errors from @Action
+config.rawError = true;
+
+// Empty (use dyanamic mode)
+export default new Vuex.Store({});
